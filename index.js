c1e202df9ca97ecf80945663608bcd9b1eb49a6b
Marco Grubert <marco.grubert@disney.com>
2014-07-22 16:44:14 -0700

    span parsing

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ readFile('U7/static/faces.vga');
 
 function readFile(filename) {
     var request = new XMLHttpRequest();
-
+    
     request.open( 'GET', filename, true );
     request.responseType = 'arraybuffer';
-
+    
     request.onload = function() {
         console.log("file loaded");
         parseFlxShp( request.response );
     }
-
+    
     request.send();
 }
 
@@ -23,78 +23,99 @@ function hex(value) {
 
 // File format from here:
 // http://wiki.ultimacodex.com/wiki/Ultima_VII_Internal_Formats
-function parseFlx(buffer) {
+function parseFlxShp(buffer) {
     var TFlxHdr = Struct.create(
-        Struct.string("comment",80),
-        Struct.uint32("magic1"),
-        Struct.uint32("numRecords"),
-        Struct.array("magic2", Struct.uint32(),10)       
-    );
+                                Struct.string("comment",80),
+                                Struct.uint32("magic1"),
+                                Struct.uint32("numRecords"),
+                                Struct.array("magic2", Struct.uint32(),10)
+                                );
     var TFlxRecordIndex = Struct.create(
-        Struct.uint32("byteOffset"), // relative to beginning of flx file
-        Struct.uint32("byteLength")
-    );
+                                        Struct.uint32("byteOffset"), // relative to beginning of flx file
+                                        Struct.uint32("byteLength")
+                                        );
     var hdr= TFlxHdr.readStructs(buffer, 0, 1)[0];
     console.log('Magic1 should be 0xffff 1a00. Found: ' + hex(hdr.magic1));
     console.log('Num records in file: '+hdr.numRecords);
     // Read all record indices
     var recordIndex = TFlxRecordIndex.readStructs(buffer, 0x80, hdr.numRecords);
     for (var i=0; i<10; ++i) {
-        console.log( i + ': ' + hex(recordIndex[i].byteOffset)+ ' ' + hex(recordIndex[i].byteLength) );
+//        console.log( i + ': ' + hex(recordIndex[i].byteOffset)+ ' ' + hex(recordIndex[i].byteLength) );
+//        parseShp(buffer, recordIndex[i].byteOffset);
     }
     parseShp(buffer, recordIndex[0].byteOffset);
 }
 
 function parseShp(buffer, fileOffset) {
+    var totalLength         = new Uint32Array(buffer, fileOffset+ 0,1)[0];
+    var firstFrameOffset    = new Uint32Array(buffer, fileOffset+ 4,1)[0];
+    var numFrames = (firstFrameOffset - 4) / 4;
     var TShpHdr = Struct.create(
-        Struct.uint32("totalLength"),
-        Struct.uint32("offset") // first data entry is at this byte offset relative to "totalLength" above
-    );
-    var shpHdr = TShpHdr.readStructs(buffer, fileOffset, 1)[0];
-    var lastOffset = Uint32Array(buffer, fileOffset+ shpHdr.offset
+                                    Struct.uint32("totalLength"),
+                                    Struct.array("offsetFrames", Struct.uint32(), numFrames) // relative to fileOffset
+                                    );
+    var frameIndex = TShpHdr.readStructs(buffer, fileOffset, 1)[0].offsetFrames;
+    console.log('numFrames: '+ numFrames);
+    for (var i=0; i<numFrames; ++i) {
+//        console.log( i + ' frame at offset ' + hex(frameIndex[i]) );
+//        parseSpanDescription(buffer, fileOffset+frameIndex[i]);
+    }
+        parseSpanDescription(buffer, fileOffset+frameIndex[0]);
 }
 
-/*
-function parseArrayBuffer(buffer) {
-    // Define the struct layout
-    var SimpleStruct = Struct.create(
-        Struct.int8("myChar"),
-        Struct.int16("myShort"),
-        Struct.int32("myInt"),
-        Struct.float32("myFloat")
-    );
+function parseSpanDescription(buffer, fileOffset)
+{
+    var TFrameDesc = Struct.create(
+                                Struct.uint16("maxX"),
+                                Struct.uint16("minXinverted"),
+                                  Struct.uint16("maxY"),
+                                  Struct.uint16("minYinverted")
+                                );
+    var frameDesc = TFrameDesc.readStructs(buffer, fileOffset, 1)[0];
+    var imageWidth = frameDesc.maxX + frameDesc.minXinverted + 1;
+    var imageHeight =frameDesc.maxY + frameDesc.minYinverted + 1;
+    console.log( 'Span ' + (frameDesc.minXinverted*-1) + ',' + (frameDesc.minYinverted*-1)  +' / '+ (frameDesc.maxX) + ',' + frameDesc.maxY);
 
-    var ComplexStruct = Struct.create(
-        Struct.struct("myStruct", SimpleStruct), // Structs can be nested
-        Struct.string("myString", 4),
-        Struct.array("myArray", Struct.int8(), 4), // Primitives or other structs can be read as fixed-length arrays
-        Struct.array("myStructArray", SimpleStruct, 2),
-        { 
-            // The last argument passed to Struct.create can be additional properties for the object
-            // These properties will be available on every instance of this struct that is created
-            myFunction: {
-                value: function () {
-                    console.log("myFunction has been called");
+    var TSpan = Struct.create(
+                                  Struct.uint16("blockData"),
+                                  Struct.int16("x"),
+                                  Struct.int16("y")
+                                  );
+    var readPointer=fileOffset+8;
+    while (true) {
+        var currSpan = TSpan.readStructs(buffer, readPointer, 1)[0]; readPointer += 6;
+        if (currSpan.blockData==0)
+            break;
+        var blockLen = currSpan.blockData >> 1;
+        var isUncompressed = (currSpan.blockData & 1) == 0;
+        if (isUncompressed) {
+            var data = new Uint8Array(buffer, readPointer, blockLen); readPointer += blockLen;
+            //            readImage(currSpan.x, currSpan.y, data);
+        } else {
+            // Run Length Encoded
+            var offsetX= currSpan.x;
+            var endX = offsetX + BlockLength;
+            
+            while (offsetX < endX) {
+                var RLEHeader = new Uint8Array(buffer, readPointer, 1)[0]; readPointer += 1;
+                var RLELength = RLEHeader >> 1;
+                var RLEuncompressed = (RLEHeader & 1)==0;
+                
+                if(RLEuncompressed) {
+                    var data = new Uint8Array(buffer, readPointer, RLELength); readPointer += RLELength;
+                    //readImage(offsetX, offsetY, data);
+                } else {
+                    var color = new Uint8Array(buffer, readPointer, 1)[0]; readPointer += 1;
+                    var data = new Uint8Array(RLELength);
+                    for (var c=0; c<RLELength; ++c) {
+                        data[c]=color;
+                    }
+//                    readImage(offsetX, currSpan.y, data);
                 }
+                
+                offsetX += RLELength;
             }
         }
-    );
-
-    // readStructs accepts the following arguments:
-    //  arrayBuffer - the ArrayBuffer to read from
-    //  offset - the byte offset into the buffer where reading should start
-    //  count - the number of structs to read. Structs are assumed to be tightly packed
-    // returns an array of structs
-    var a = SimpleStruct.readStructs(buffer, 0, 2); // Returns an array of 2 simpleStructs
-    var b = ComplexStruct.readStructs(buffer, 32, 1); // Returns an array of 1 complexStruct
-
-    // myFunction will be available on every instance of a ComplexStruct
-    b[0].myFunction();
-
-    // readStructs can also accept a callback, which will be called with the parsed structure and offset of that 
-    // structure within the stream as they are parsed.
-    SimpleStruct.readStructs(buffer, 0, 2, function (newStruct, offset) {
-        console.log("Parsed " + newStruct + " at offset " + offset);
-    });
-
-}*/
+        break;
+    }
+}
