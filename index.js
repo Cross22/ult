3b1e5ed48c43c855d8c45dbd10671a4a61257f04
Marco Grubert <marco.grubert@disney.com>
2014-07-22 18:03:34 -0700

    RLE reading works. no palettes

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 /*jslint node: true, vars:true */
 "use strict";
 
-readFile('U7/static/faces.vga');
+var ctx ;
+
+window.onload=function(){
+    var canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+
+    var SCALE=2;
+    var col=200;
+//    ctx.fillStyle="rgb("+col+","+col+",0)";//200,0,0)";//""#FF0000";//"rgb("+data[i]+","+data[i]+","+data[i]+")";
+//    ctx.fillRect(0*SCALE,0*SCALE,SCALE,SCALE);
+//
+//    ctx.fillRect(20*SCALE,20*SCALE,SCALE,SCALE);
+
+    readFile('U7/static/faces.vga');
+};
+
 
 function readFile(filename) {
     var request = new XMLHttpRequest();
@@ -63,18 +78,36 @@ function parseShp(buffer, fileOffset) {
         parseSpanDescription(buffer, fileOffset+frameIndex[0]);
 }
 
+var imgLeft, imgRight,imgTop, imgBottom;
+function newImage(left,top,right,bottom) {
+    var imageWidth = right -left + 1;
+    var imageHeight =bottom - top +1;
+    imgLeft= left; imgRight= right; imgTop= top; imgBottom= bottom;
+}
+
+function readImage(x, y, data) {
+    var SCALE=4;
+    x+= -imgLeft; y+= -imgTop;
+    for (var i=0; i<data.length; ++i) {
+        var color= data[i];
+        ctx.fillStyle="rgb("+color+","+color+","+0+")";
+        ctx.fillRect(x*SCALE,y*SCALE,SCALE,SCALE);
+        ++x;
+    }
+}
+
 function parseSpanDescription(buffer, fileOffset)
 {
     var TFrameDesc = Struct.create(
                                 Struct.uint16("maxX"),
                                 Struct.uint16("minXinverted"),
-                                  Struct.uint16("maxY"),
-                                  Struct.uint16("minYinverted")
+                                  Struct.uint16("minYinverted"),
+                                   Struct.uint16("maxY")
                                 );
     var frameDesc = TFrameDesc.readStructs(buffer, fileOffset, 1)[0];
-    var imageWidth = frameDesc.maxX + frameDesc.minXinverted + 1;
-    var imageHeight =frameDesc.maxY + frameDesc.minYinverted + 1;
-    console.log( 'Span ' + (frameDesc.minXinverted*-1) + ',' + (frameDesc.minYinverted*-1)  +' / '+ (frameDesc.maxX) + ',' + frameDesc.maxY);
+    newImage(-frameDesc.minXinverted, -frameDesc.minYinverted, frameDesc.maxX, frameDesc.maxY);
+    
+    console.log( 'Frame ' + (frameDesc.minXinverted*-1) + ',' + (frameDesc.minYinverted*-1)  +' / '+ (frameDesc.maxX) + ',' + frameDesc.maxY);
 
     var TSpan = Struct.create(
                                   Struct.uint16("blockData"),
@@ -90,11 +123,11 @@ function parseSpanDescription(buffer, fileOffset)
         var isUncompressed = (currSpan.blockData & 1) == 0;
         if (isUncompressed) {
             var data = new Uint8Array(buffer, readPointer, blockLen); readPointer += blockLen;
-            //            readImage(currSpan.x, currSpan.y, data);
+            readImage(currSpan.x, currSpan.y, data);
         } else {
             // Run Length Encoded
             var offsetX= currSpan.x;
-            var endX = offsetX + BlockLength;
+            var endX = offsetX + blockLen;
             
             while (offsetX < endX) {
                 var RLEHeader = new Uint8Array(buffer, readPointer, 1)[0]; readPointer += 1;
@@ -103,19 +136,18 @@ function parseSpanDescription(buffer, fileOffset)
                 
                 if(RLEuncompressed) {
                     var data = new Uint8Array(buffer, readPointer, RLELength); readPointer += RLELength;
-                    //readImage(offsetX, offsetY, data);
+                    readImage(offsetX, currSpan.y, data);
                 } else {
                     var color = new Uint8Array(buffer, readPointer, 1)[0]; readPointer += 1;
                     var data = new Uint8Array(RLELength);
                     for (var c=0; c<RLELength; ++c) {
                         data[c]=color;
                     }
-//                    readImage(offsetX, currSpan.y, data);
+                    readImage(offsetX, currSpan.y, data);
                 }
                 
                 offsetX += RLELength;
             }
         }
-        break;
     }
 }
